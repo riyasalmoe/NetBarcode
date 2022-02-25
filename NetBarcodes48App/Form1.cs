1b1703537fc84cb5dfb55730baae20a129e1f91a
Riyas Aboobaker <riyas.almoe@gmail.com>
2022-02-25 11:44:27 +0400

    compiled it for .Net Framework 4.8, Included Test application

---
+++ NetBarcodes48App/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NetBarcodeNetBarcodes48;
+
+namespace NetBarcodes48App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var barcode = new Barcode("123456789", NetBarcodeNetBarcodes48.Type.Code128B);
+            pictureBox1.Image = barcode.GetImage();
+
+        }
+    }
+}
